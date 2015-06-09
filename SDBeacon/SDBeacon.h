4688d48102dd122c3ab4670f74cbe0f84a0e8398
mikealbinson <michael_albinson@ticketfly.com>
2015-06-09 10:14:10 -0700

    Update SDBeacon.h

--- SDBeacon/SDBeacon.h
+++ SDBeacon/SDBeacon.h
@@ -1,6 +1,6 @@
 /*
-* SDBeacon.h
-*
+* SDBeacon.h By Michael Albinson
+* Based on the SdFat library
 *
 */
 
@@ -9,28 +9,43 @@
 #define SDBeacon_h
 
 #include "Arduino.h"
-#include <SdFatMod.h> //THIS FILE REQUIRES A MODIFIED VERSION OF <SD.h>
+#include <SdFatMod.h> //THIS FILE REQUIRES A MODIFIED VERSION OF <SdFat.h>
 #include <SPI.h>
 
 #define maxTitleSize 16
 
 class TicketflySDBeacon{
-public:
-	TicketflySDBeacon();
-	void SDBeaconSetup(SdFat& SD); //go here to change files that are being searched for each type
-	String textFileSelect ();
-	String TicketInfoParser (String TicketString, int typeReturn); 
-	bool ticketCompare (String searchFor, const char* compareToThis);
-	bool ticketCompare (const char* searchFor, String compareToThis);
-	bool ticketCompare (String searchFor, String compareToThis);
-	bool searchDatabaseForTicket(String TicketString, SdFat& SD);
-	void writeToSD(String writeThisString, String fileName, SdFat& SD);
-	void simpleReadSD (String fileName, SdFat& SD);
-	void fileRemove (String fileName, SdFat& SD);
-	bool parseTextLines (String TicketRecieved, int receiveType, SdFat& SD);
+
 	
-private:
-	SdFat SDFatObj;
+public:
+	TicketflySDBeacon(); //TicketflySDBeacon object constructor
+
+	void SDBeaconSetup(SdFat& SD); //go here to change files that are being searched for each type--ONLY USE IN THE SETUP LOOP
+
+	String textFileSelect (); //Takes in input from the serial monitor and outputs a String that can be used in the following functions to declare
+							  //the file(s) you would like to use. It is suggested that this be implemented in the setup function.
+
+	bool searchDatabaseForTicket(String TicketString, SdFat& SD); //One method to search for a ticket in your SD database, returns true
+																  //if the ticket is found
+
+	void writeToSD(String writeThisString, String fileName, SdFat& SD); //Writes a string to the file indicated by "fileName". 
+
+	void simpleReadSD (String fileName, SdFat& SD); //Reads the entirety of the file indicated by "fileName"
+
+	void fileRemove (String fileName, SdFat& SD); //removes the fiel indicated by "fileName" --NOTE: this file cannot be recovered after it is removed
+
+	bool ticketCompare (String searchFor, String compareToThis); //A method for parseTextLines that compares twp tickets and returns true
+																 //if they are the same
+
+	String TicketInfoParser (String TicketString, int typeReturn); //a method for parseTextLines that pulls reads a file line by line
+																   //to a string. Returns the string of either the number, name or type
+																   //of ticket (must be separated by commas in the file)
+
+	String parseTextLines (String TicketRecieved, int receiveType, SdFat& SD, TicketflySDBeacon& SDB); //combines the above two functions to
+																									   //compare the "TicketReceived"
+																									   //string to the document to check 
+																									   //if it exists on the SD file
+
 };
 
 #endif
