997aa53e775c83f658cae6f3555e250be6d4a479
mikealbinson <michael_albinson@ticketfly.com>
2015-06-09 11:30:01 -0700

    Update mocks.h

--- mocks/mocks.h
+++ mocks/mocks.h
@@ -15,12 +15,12 @@ void endOfScenario ();
 
 class MockFile{
 public:
-	MockFile();
-	bool read();
-	const char* read (const char* interest, int that);
-	bool close();
-	String println(String lineToPrint);
-	bool available();
+	MockFile(); //Constructor for the mockFile class
+	bool read(); //a read function that evuates to true
+	const char* read (const char* interest, int that); // a secondary read function that evaluates to true
+	bool close(); //a close function mock up that evaluates to true
+	String println(String lineToPrint); //a println mock function that returns the string printed
+	bool available(); //a mock available function that evaluates to true
 	
 private:
 	String readerString = "HelloYouThere";
@@ -28,51 +28,51 @@ private:
 
 class MockSdFat {
  public:
-	MockSdFat();
-	bool open(String file);
-	bool open(String file, const uint8_t function);
-	bool begin(int i);
-	bool remove (String file);
-	bool available(); 
+	MockSdFat(); //Constructor for the MockSdFat class
+	bool open(String file); //a mock open function that evaluates to true
+	bool open(String file, const uint8_t function); //a secondary mock open function that evaluates to true
+	bool begin(int i);//a mock begin function that evaluates to true
+	bool remove (String file);//a mock remove function that evaluates to true
+	bool available(); //a mock available function that evaluates to true
 
 };
 
 class MockSDBeacon{
 public:
-	  MockSDBeacon();
-		void SDBeaconSetup (MockSdFat& SD);
-		bool searchDatabaseForTicket(String TicketString, MockSdFat& SD,  MockFile& myFile);
-		void writeToSD (String writeThisString, String fileName, MockSdFat& SD, MockFile& myFile);
-		void simpleReadSD (String fileName, MockSdFat& SD, MockFile& myFile);
-		void fileRemove (String fileName, MockSdFat& SD, MockFile& myFile);
+	  MockSDBeacon(); //Constructor for the MockSDBeacon class
+		void SDBeaconSetup (MockSdFat& SD); //a mock setup function that includes a test to demonstrate if the file opens or not
+		bool searchDatabaseForTicket(String TicketString, MockSdFat& SD,  MockFile& myFile); //a mock search function that evaluates to true
+		void writeToSD (String writeThisString, String fileName, MockSdFat& SD, MockFile& myFile); //a mock write function that includes a test to demonstrate if the file is written to or not
+		void simpleReadSD (String fileName, MockSdFat& SD, MockFile& myFile);//a mock read function that includes a test to demonstrate if the file is read or not
+		void fileRemove (String fileName, MockSdFat& SD, MockFile& myFile);//a mock remove function that includes a test to demonstrate if the file is removed or not
 };
 
 class MockOLED{
 	private:
-	U8GLIB_SSD1306_128X32MOCK *MOCKu8g;
-	bool exists;
+	U8GLIB_SSD1306_128X32MOCK *MOCKu8g; //constructor for the MOCKu8g object
+	bool exists; //used internally
 		
 	
   public:
-		int MockPrintToOLED (String printThis);
-		int setColorIndexMock(int type);
-  	MockOLED();
-		void drawSetupMock(MockOLED &obj);
-  	const char* drawGeneralMock(String stringToPrint, MockOLED &obj);
+		int MockPrintToOLED (String printThis); // a mock print function for the OLED
+		int setColorIndexMock(int type); // a mock setIndex function for the OLED
+  	MockOLED(); //Constructor for the MockOLED class
+		void drawSetupMock(MockOLED &obj); //a mock setup function for the OLED
+  	const char* drawGeneralMock(String stringToPrint, MockOLED &obj); // a mock general draw function for the OLED
 };
 
 class MockBLE {
 public:
-	MockBLE();
-	void TflyBLESetup ();
+	MockBLE(); //Constructor for the MockBLE class
+	void TflyBLESetup ();//a mock setup function for the BLE
 	void clearParsingSetups(String stringToClear, char charArrayToClear[]);
-	void beaconSendData();
-	String readIncoming();
+	void beaconSendData();//a mock send function for the BLE
+	String readIncoming();//a mock receive function for the BLE
 	
 private:
 	int ble_available = true; //let's assume we're costantly connected
-	char MockBLE_Read(int &i);
-	const char* MockBLE_write (int carrier, int &i);
+	char MockBLE_Read(int &i); // an interior read method that allows for simulated reading
+	const char* MockBLE_write (int carrier, int &i); // an interior method for simulated writing
 	
 };
 
