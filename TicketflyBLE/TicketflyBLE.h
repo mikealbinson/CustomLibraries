1ecc5a547bbcdb5b36d67002462c6f886b6d4ffe
mikealbinson <michael_albinson@ticketfly.com>
2015-06-09 09:30:46 -0700

    Update TicketflyBLE.h

--- TicketflyBLE/TicketflyBLE.h
+++ TicketflyBLE/TicketflyBLE.h
@@ -1,8 +1,9 @@
 /*
-* Work in Progress
-*
-* NOTE: If you don't include one of the body (loop) functions, you MUST add ble_do_events() to the body of your 
-* loop function, otherwise the BLE will not initialize
+* TicketflyBLE.h by Michael Albinson
+* Based on the RBL nRF80001 library
+* NOTE: If you don't include one of the body (loop) functions (either beaconSendData or readIncoming), you MUST add ble_do_events() to the body of your 
+* loop function, otherwise the BLE will not initialize (although it will not do anything without either of these functions... So if you
+* do not plan to use them you may want to consider not using this library)
 */
 
 #ifndef TICKETFLYBLE_H_
@@ -13,23 +14,15 @@
 #include <SPI.h>
 #include <boards.h>
 #include <RBL_LibCLONE.h>
-#include <Testing.h>
 
 #define CHARS_TO_READ 14 //Set number of char to read from the BLE (max of 16)
 
 class BLE {
 public:
-	BLE();
-	BLE (bool enterMockFlag);
-	void TflyBLESetup ();
-	void startBLEScan();//currently empty
-	void clearParsingSetups(String stringToClear, char charArrayToClear[]);
-	void beaconSendData();
-	void beaconRequestInfo();
-	String readIncoming();
-	
-private:
-	int mockFlag;
+	BLE();  //Constructor for the BLE object
+	void TflyBLESetup (); //Initializes the BLE 
+	void beaconSendData(); //Tells the BLE beacon to send data typed into the serial monitor
+	String readIncoming(); //tells the BLE beacon to receive data sent from the RBL BLE app-->returns it as a string
 };
 
 #endif
